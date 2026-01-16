d09e1fea9cb067116f8949a78b0b84ea5eb5fefb
muhammadalfin9 <muhamadalfin187@gmail.com>
2026-01-16 19:59:17 +0700

    done

---
+++ quiz/part3/soal1.js
@@ -0,0 +1,34 @@
+function changeMe(arr) {
+  // you can only write your code here!
+
+  for (let i = 0; i < arr.length; i++) {
+    const personal = {};
+    personal["firstName"] = arr[i][0];
+    personal["lastName"] = arr[i][1];
+    personal["gender"] = arr[i][2];
+    if (arr[i][3] == undefined) {
+      personal.age = "Invalid Birth Year";
+    } else {
+      personal.age = 2023 - arr[i][3];
+    }
+    console.log(personal);
+  }
+}
+
+// TEST CASES
+changeMe([
+  ["Christ", "Evans", "Male", 1982],
+  ["Robert", "Downey", "Male"],
+]); // 1. Christ Evans:
+// Christ Evans: { firstName: 'Christ',
+//   lastName: 'Evans',
+//   gender: 'Male',
+//   age: 41 } 2023 - 1982 = 41 kan yak wkwk
+// Robert Downey: { firstName: 'Robert',
+//   lastName: 'Downey',
+//   gender: 'Male',
+//   age: 'Invalid Birth Year' }
+
+//intinya bagaimana cara kalian bisa menampilkan output diatas, sebebas dan sekreatif kalian.
+
+changeMe([]); // ""
